c1bc16c5957e2ca110804ff00f23f962210f2f1a
tracer-ics2017 <tracer@njuics.org>
2026-04-27 22:35:21 -0700

    >  compile 161220000 cong Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  22:35:21 up 12:01,  5 users,  load average: 0.01, 0.02, 0.00 7afb0acc705d574369fdef448ed907e138e09f2

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -5,10 +5,19 @@
  */
 #include <sys/types.h>
 #include <regex.h>
+#include <stdlib.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ
-
+  TK_NOTYPE = 256, 
+  TK_EQ,
+  TK_NEQ,  // !=
+  TK_AND,  // &
+  TK_OR,   // |
+  TK_NUM,  // Number
+  TK_HEX,  // Hex number
+  TK_REG,  // Register
+  TK_LPAREN, // (
+  TK_RPAREN  // )
   /* TODO: Add more token types */
 
 };
@@ -24,7 +33,19 @@ static struct rule {
 
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
-  {"==", TK_EQ}         // equal
+  {"-", '-'},             // minus
+  {"\\*", '*'},           // multiply
+  {"/", '/'},             // divide
+  {"%", '%'},             // modulo
+  {"==", TK_EQ},         // equal
+  {"!=", TK_NEQ},         // not equal
+  {"&", TK_AND},          // bitwise and
+  {"\\|", TK_OR},         // bitwise or
+  {"\\(", TK_LPAREN},     // left parenthesis
+  {"\\)", TK_RPAREN},     // right parenthesis
+  {"0[xX][0-9a-fA-F]+", TK_HEX},  // hex number
+  {"[0-9]+", TK_NUM},     // decimal number
+  {"\\$[a-zA-Z]+", TK_REG},      // register
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -80,7 +101,30 @@ static bool make_token(char *e) {
          */
 
         switch (rules[i].token_type) {
-          default: TODO();
+          case TK_NOTYPE:
+            break;
+          case TK_NUM:
+          case TK_HEX:
+          case TK_REG:
+          case '+':
+          case '-':
+          case '*':
+          case '/':
+          case '%':
+          case TK_EQ:
+          case TK_NEQ:
+          case TK_AND:
+          case TK_OR:
+          case TK_LPAREN:
+          case TK_RPAREN:
+            tokens[nr_token].type = rules[i].token_type;
+            strncpy(tokens[nr_token].str, substr_start, substr_len);
+            tokens[nr_token].str[substr_len] = '\0';
+            nr_token++;
+            break;
+          default:
+            printf("unknown token type: %d\n", rules[i].token_type);
+            return false;
         }
 
         break;
@@ -96,6 +140,168 @@ static bool make_token(char *e) {
   return true;
 }
 
+static uint32_t eval(int p, int q, bool *success)
+{
+  if (p == q)
+  {
+    if (tokens[p].type == TK_NUM)
+    {
+      return strtoul(tokens[p].str, NULL, 10);
+    }
+    else if (tokens[p].type == TK_HEX)
+    {
+      return strtoul(tokens[p].str, NULL, 16);
+    }
+    else if (tokens[p].type == TK_REG)
+    {
+      if (strcmp(tokens[p].str, "$eax") == 0)
+        return cpu.eax;
+      if (strcmp(tokens[p].str, "$ecx") == 0)
+        return cpu.ecx;
+      if (strcmp(tokens[p].str, "$edx") == 0)
+        return cpu.edx;
+      if (strcmp(tokens[p].str, "$ebx") == 0)
+        return cpu.ebx;
+      if (strcmp(tokens[p].str, "$esp") == 0)
+        return cpu.esp;
+      if (strcmp(tokens[p].str, "$ebp") == 0)
+        return cpu.ebp;
+      if (strcmp(tokens[p].str, "$esi") == 0)
+        return cpu.esi;
+      if (strcmp(tokens[p].str, "$edi") == 0)
+        return cpu.edi;
+      if (strcmp(tokens[p].str, "$eip") == 0)
+        return cpu.eip;
+      // Add more registers as needed
+      printf("unknown register: %s\n", tokens[p].str);
+      *success = false;
+      return 0;
+    }
+    printf("unexpected token type: %d\n", tokens[p].type);
+    *success = false;
+    return 0;
+  }
+
+  if (tokens[p].type == '(' && tokens[q].type == ')')
+  {
+    return eval(p + 1, q - 1, success);
+  }
+
+  int op_pos = -1;
+  int paren_level = 0;
+  int precedence = 0;
+
+#define PREC_ADD_SUB 1
+#define PREC_MUL_DIV_MOD 2
+#define PREC_EQ 3
+#define PREC_AND 4
+#define PREC_OR 5
+
+  for (int i = p; i <= q; i++)
+  {
+    int t = tokens[i].type;
+
+    if (t == '(')
+    {
+      paren_level++;
+    }
+    else if (t == ')')
+    {
+      paren_level--;
+    }
+    else if (paren_level == 0)
+    {
+      int curr_prec = 0;
+
+      if (t == '+' || t == '-')
+      {
+        curr_prec = PREC_ADD_SUB;
+      }
+      else if (t == '*' || t == '/' || t == '%')
+      {
+        curr_prec = PREC_MUL_DIV_MOD;
+      }
+      else if (t == TK_EQ || t == TK_NEQ)
+      {
+        curr_prec = PREC_EQ;
+      }
+      else if (t == TK_AND)
+      {
+        curr_prec = PREC_AND;
+      }
+      else if (t == TK_OR)
+      {
+        curr_prec = PREC_OR;
+      }
+
+      if (curr_prec > precedence)
+      {
+        precedence = curr_prec;
+        op_pos = i;
+      }
+    }
+  }
+
+  if (op_pos == -1)
+  {
+    if (tokens[p].type == '+' || tokens[p].type == '-')
+    {
+      uint32_t val = eval(p + 1, q, success);
+      if (!(*success))
+        return 0;
+      return (tokens[p].type == '+') ? val : -val;
+    }
+    printf("no dominant operator found\n");
+    *success = false;
+    return 0;
+  }
+
+  uint32_t left = eval(p, op_pos - 1, success);
+  if (!(*success))
+    return 0;
+  uint32_t right = eval(op_pos + 1, q, success);
+  if (!(*success))
+    return 0;
+
+  switch (tokens[op_pos].type)
+  {
+  case '+':
+    return left + right;
+  case '-':
+    return left - right;
+  case '*':
+    return left * right;
+  case '/':
+    if (right == 0)
+    {
+      printf("division by zero\n");
+      *success = false;
+      return 0;
+    }
+    return left / right;
+  case '%':
+    if (right == 0)
+    {
+      printf("modulo by zero\n");
+      *success = false;
+      return 0;
+    }
+    return left % right;
+  case TK_EQ:
+    return left == right;
+  case TK_NEQ:
+    return left != right;
+  case TK_AND:
+    return left & right;
+  case TK_OR:
+    return left | right;
+  default:
+    printf("unknown operator: %d\n", tokens[op_pos].type);
+    *success = false;
+    return 0;
+  }
+}
+
 uint32_t expr(char *e, bool *success) {
   if (!make_token(e)) {
     *success = false;
@@ -103,7 +309,6 @@ uint32_t expr(char *e, bool *success) {
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  TODO();
-
-  return 0;
+  *success = true;
+  return eval(0, nr_token - 1, success);
 }
