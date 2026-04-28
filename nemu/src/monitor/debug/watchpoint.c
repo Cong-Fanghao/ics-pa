16203b2354d760b488de4626864a28426ecf11e5
tracer-ics2017 <tracer@njuics.org>
2026-04-28 03:14:17 -0700

    >  compile 161220000 cong Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  03:14:17 up 16:40,  5 users,  load average: 0.17, 0.05, 0.01 f95adae4a32950531f631f428c05b8dd5d0ea9f

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -20,4 +20,113 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
+WP *new_wp()
+{
+  if (free_ == NULL)
+  {
+    assert(0);
+    return NULL;
+  }
 
+  WP *wp = free_;
+  free_ = free_->next;
+
+  wp->next = head;
+  head = wp;
+
+  wp->enabled = true;
+  return wp;
+}
+
+void free_wp(WP *wp)
+{
+  if (wp == NULL)
+    return;
+
+  WP **pp = &head;
+  while (*pp != NULL && *pp != wp)
+  {
+    pp = &(*pp)->next;
+  }
+
+  if (*pp == wp)
+  {
+    *pp = wp->next;
+  }
+
+  wp->expr[0] = '\0';
+  wp->old_val = 0;
+  wp->enabled = false;
+  wp->next = free_;
+  free_ = wp;
+}
+
+void print_watchpoints()
+{
+  if (head == NULL)
+  {
+    printf("No watchpoints.\n");
+    return;
+  }
+
+  printf("Num     Type           Disp Enb Address    What\n");
+  WP *wp = head;
+  while (wp != NULL)
+  {
+    printf("%-8dwatchpoint     keep y   %-10s %s\n",
+           wp->NO, "", wp->expr);
+    wp = wp->next;
+  }
+}
+
+bool check_watchpoints()
+{
+  WP *wp = head;
+  bool triggered = false;
+
+  while (wp != NULL)
+  {
+    if (wp->enabled && wp->expr[0] != '\0')
+    {
+      bool success = true;
+      uint32_t new_val = expr(wp->expr, &success);
+
+      if (!success)
+      {
+        printf("Warning: Failed to evaluate watchpoint %d expression: %s\n",
+               wp->NO, wp->expr);
+        wp = wp->next;
+        continue;
+      }
+
+      if (new_val != wp->old_val)
+      {
+        printf("Watchpoint %d: %s\n\n", wp->NO, wp->expr);
+        printf("Old value = 0x%08x (%d)\n", wp->old_val, (int32_t)wp->old_val);
+        printf("New value = 0x%08x (%d)\n", new_val, (int32_t)new_val);
+
+        wp->old_val = new_val;
+        triggered = true;
+      }
+    }
+    wp = wp->next;
+  }
+
+  return triggered;
+}
+
+void delete_watchpoint(int no)
+{
+  WP *wp = head;
+  while (wp != NULL)
+  {
+    if (wp->NO == no)
+    {
+      free_wp(wp);
+      printf("Watchpoint %d deleted.\n", no);
+      return;
+    }
+    wp = wp->next;
+  }
+  printf("No watchpoint number %d.\n", no);
+}
