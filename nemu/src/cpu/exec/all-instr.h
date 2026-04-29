9add455141feb966c325a825ddd654d51b2833ec
tracer-ics2017 <tracer@njuics.org>
2026-04-29 02:32:49 -0700

    >  compile 161220000 cong Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  02:32:49 up  2:23,  1 user,  load average: 0.00, 0.01, 0.00 d099a6d347b18c7325712be19e5eed933dab5f49

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -9,3 +9,7 @@ make_EHelper(nemu_trap);
 
 make_EHelper(sub);
 make_EHelper(call);
+make_EHelper(push);
+make_EHelper(pop);
+make_EHelper(xor);
+make_EHelper(ret);
--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -6,14 +6,15 @@ make_EHelper(mov) {
 }
 
 make_EHelper(push) {
-  TODO();
-
+  // TODO();
+  rtl_push(&id_src->val);
   print_asm_template1(push);
 }
 
 make_EHelper(pop) {
-  TODO();
-
+  // TODO();
+  rtl_pop(&id_dest->val);
+  rtl_sr(id_dest->reg, id_dest->width, &id_dest->val);
   print_asm_template1(pop);
 }
 
