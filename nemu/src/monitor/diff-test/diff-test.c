ccf18f522b3b787d92b6246ccaa4f1b68616df1a
tracer-ics2017 <tracer@njuics.org>
2026-04-29 22:37:46 -0700

    >  compile 2310682 cong Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:37:46 up 14:13,  1 user,  load average: 0.45, 0.79, 0.65 154d5780f6f49fe4475e970c9f79338297389186

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -150,29 +150,28 @@ void difftest_step(uint32_t eip) {
   // TODO: Check the registers state with QEMU.
   // Set `diff` as `true` if they are not the same.
   // TODO();
-  if (cpu.eax != r.eax)
+  if (r.eax != cpu.eax ||
+      r.ecx != cpu.ecx ||
+      r.edx != cpu.edx ||
+      r.ebx != cpu.ebx ||
+      r.esp != cpu.esp ||
+      r.ebp != cpu.ebp ||
+      r.esi != cpu.esi ||
+      r.edi != cpu.edi ||
+      r.eip != cpu.eip)
+  {
     diff = true;
-  if (cpu.ecx != r.ecx)
-    diff = true;
-  if (cpu.edx != r.edx)
-    diff = true;
-  if (cpu.ebx != r.ebx)
-    diff = true;
-  if (cpu.esp != r.esp)
-    diff = true;
-  if (cpu.ebp != r.ebp)
-    diff = true;
-  if (cpu.esi != r.esi)
-    diff = true;
-  if (cpu.edi != r.edi)
-    diff = true;
-
-  if (cpu.eip != r.eip)
-    diff = true;
-
-  uint32_t mask = 0x00000c8b;
-  if ((cpu.eflags.val & mask) != (r.eflags & mask))
-    diff = true;
+    printf("Exception: NEMU EIP 0x%08X, QEMU EIP 0x%08X.\n", cpu.eip, r.eip);
+    printf("\tQEMU\t\tNEMU\n");
+    printf("EAX = 0x%08X, 0x%08X\n", r.eax, cpu.eax);
+    printf("ECX = 0x%08X, 0x%08X\n", r.ecx, cpu.ecx);
+    printf("EDX = 0x%08X, 0x%08X\n", r.edx, cpu.edx);
+    printf("EBX = 0x%08X, 0x%08X\n", r.ebx, cpu.ebx);
+    printf("ESP = 0x%08X, 0x%08X\n", r.esp, cpu.esp);
+    printf("EBP = 0x%08X, 0x%08X\n", r.ebp, cpu.ebp);
+    printf("ESI = 0x%08X, 0x%08X\n", r.esi, cpu.esi);
+    printf("EDI = 0x%08X, 0x%08X\n", r.edi, cpu.edi);
+  }
 
   if (diff) {
     nemu_state = NEMU_END;
