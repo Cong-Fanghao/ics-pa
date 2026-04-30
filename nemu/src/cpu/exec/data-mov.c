4d5fe87a756570f9e2d45ca15263d85853505e99
tracer-ics2017 <tracer@njuics.org>
2026-04-29 18:08:27 -0700

    >  compile 2310682 cong Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:08:27 up 10:42,  1 user,  load average: 0.37, 0.22, 0.12 4363173c99b8d25d9d06d906314cfb4ce026af58

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -1,43 +1,36 @@
 #include "cpu/exec.h"
 
-make_EHelper(mov)
-{
+make_EHelper(mov) {
   operand_write(id_dest, &id_src->val);
   print_asm_template2(mov);
 }
 
-make_EHelper(push)
-{
+make_EHelper(push) {
   // TODO();
-  rtl_push(&id_dest->val);
+  rtl_push(&id_src->val);
   print_asm_template1(push);
 }
 
-make_EHelper(pop)
-{
+make_EHelper(pop) {
   // TODO();
-  rtl_pop(&t2);
-  operand_write(id_dest, &t2);
-
+  rtl_pop(&id_dest->val);
+  rtl_sr(id_dest->reg, id_dest->width, &id_dest->val);
   print_asm_template1(pop);
 }
 
-make_EHelper(pusha)
-{
+make_EHelper(pusha) {
   TODO();
 
   print_asm("pusha");
 }
 
-make_EHelper(popa)
-{
+make_EHelper(popa) {
   TODO();
 
   print_asm("popa");
 }
 
-make_EHelper(leave)
-{
+make_EHelper(leave) {
   // TODO();
   rtl_mv(&cpu.esp, &cpu.ebp);
   rtl_pop(&cpu.ebp);
@@ -45,56 +38,45 @@ make_EHelper(leave)
   print_asm("leave");
 }
 
-make_EHelper(cltd)
-{
-  if (decoding.is_operand_size_16)
-  {
-    // TODO();16->32
-    rtl_lr_w(&t0, R_AX);
-    rtl_sext(&t0, &t0, 2);
-    rtl_sari(&t0, &t0, 31);
-    rtl_sr_w(R_DX, &t0);
+make_EHelper(cltd) {
+  if (decoding.is_operand_size_16) {
+    // TODO();
+    rtl_sext(&cpu.edx, &cpu.eax, 2);
   }
-  else
-  {
-    // TODO();32->64
-    rtl_sari(&cpu.edx, &cpu.eax, 31);
+  else {
+    // TODO();
+    rtl_msb(&t0, &cpu.eax, 4);
+    rtl_sub(&cpu.edx, &tzero, &t0);
   }
 
   print_asm(decoding.is_operand_size_16 ? "cwtl" : "cltd");
 }
 
-make_EHelper(cwtl)
-{
-  if (decoding.is_operand_size_16)
-  {
+make_EHelper(cwtl) {
+  if (decoding.is_operand_size_16) {
     TODO();
   }
-  else
-  {
+  else {
     TODO();
   }
 
   print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
 }
 
-make_EHelper(movsx)
-{
+make_EHelper(movsx) {
   id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
   rtl_sext(&t2, &id_src->val, id_src->width);
   operand_write(id_dest, &t2);
   print_asm_template2(movsx);
 }
 
-make_EHelper(movzx)
-{
+make_EHelper(movzx) {
   id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
   operand_write(id_dest, &id_src->val);
   print_asm_template2(movzx);
 }
 
-make_EHelper(lea)
-{
+make_EHelper(lea) {
   rtl_li(&t2, id_src->addr);
   operand_write(id_dest, &t2);
   print_asm_template2(lea);
