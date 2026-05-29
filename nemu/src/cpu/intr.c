6798d598b464fb2abe19bf038da91dbb64faf05b
tracer-ics2017 <tracer@njuics.org>
2026-05-29 03:57:49 -0700

    >  compile 2310682 cong Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  03:57:49 up  2:08,  1 user,  load average: 0.24, 0.08, 0.02 e4161a3d5bc1700f10a2d1a0d26690506c6640e6

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -2,31 +2,28 @@
 #include "memory/mmu.h"
 
 void raise_intr(uint8_t NO, vaddr_t ret_addr) {
-  // 保存 EFLAGS
+  /* TODO: Trigger an interrupt/exception with ``NO''.
+   * That is, use ``NO'' to index the IDT.
+   */
+
+  //TODO();
   rtlreg_t temp_eflags = cpu.eflags.val;
   rtl_push(&temp_eflags);
-  
-  // 保存 CS（转换为 32 位）
   rtlreg_t temp_cs = cpu.cs;
   rtl_push(&temp_cs);
-  
-  // 保存 EIP
   rtlreg_t temp_eip = ret_addr;
   rtl_push(&temp_eip);
 
-  // 在 IDT 中找到中断号 NO 对应的门描述符首地址
-  vaddr_t gate_addr = cpu.idtr.base + NO * sizeof(GateDesc);
-  Log("gate_addr=%d, cpu.idtr.base=%d, cpu.idtr.limit=%d", 
-      gate_addr, cpu.idtr.base, cpu.idtr.limit);
-  assert(gate_addr <= cpu.idtr.base + cpu.idtr.limit);
+  //在IDT中找到中断号NO对应的门描述符首地址
+  vaddr_t gate_addr=cpu.idtr.base+NO*sizeof(GateDesc);
 
-  // 根据门描述符的 offset 计算目标地址
-  uint32_t off_15_0 = vaddr_read(gate_addr, 2);
-  uint32_t off_32_16 = vaddr_read(gate_addr + sizeof(GateDesc) - 2, 2);
-  uint32_t target_addr = (off_32_16 << 16) | off_15_0;
+  //根据门描述符的offset计算目标地址，由两部分拼接而成
+  uint32_t off_15_0=vaddr_read(gate_addr,2);
+  uint32_t off_32_16=vaddr_read(gate_addr+sizeof(GateDesc)-2,2);
+  uint32_t target_addr=(off_32_16<<16)+off_15_0;
 
-  decoding.is_jmp = 1;
-  decoding.jmp_eip = target_addr;
+  decoding.is_jmp=1;
+  decoding.jmp_eip=target_addr;
 }
 
 void dev_raise_intr() {
