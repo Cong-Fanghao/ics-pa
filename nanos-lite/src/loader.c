b01b7869488c21d6ea4645d2e7be71e5cb615f96
tracer-ics2017 <tracer@njuics.org>
2026-05-28 21:36:04 -0700

    >  compile 2310682 cong Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:36:04 up 18:06,  1 user,  load average: 0.28, 0.12, 0.04 59736ab26243e66361ed94e17fe3f5c634f2ab1

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -7,25 +7,7 @@ extern size_t get_ramdisk_size();
 
 uintptr_t loader(_Protect *as, const char *filename) {
   // TODO();
-  Elf_Ehdr elf_header;
-  
-  ramdisk_read(&elf_header, 0, sizeof(Elf_Ehdr));
-
-  uint8_t ph_buffer[elf_header.e_phentsize * elf_header.e_phnum];
-  ramdisk_read(ph_buffer, elf_header.e_phoff, sizeof(ph_buffer));
-
-  for (int j = 0; j < elf_header.e_phnum; ++j) {
-    Elf_Phdr *segment = (Elf_Phdr *)(ph_buffer + j * elf_header.e_phentsize);
-
-    if (segment->p_type == PT_LOAD) {
-      ramdisk_read((void *)segment->p_vaddr, segment->p_offset, segment->p_filesz);
-      if (segment->p_memsz > segment->p_filesz) {
-        uint32_t bss_start = segment->p_vaddr + segment->p_filesz;
-        uint32_t bss_size  = segment->p_memsz - segment->p_filesz;
-        memset((void *)bss_start, 0, bss_size);
-      }
-    }
-  }
+  ramdisk_read((void *)DEFAULT_ENTRY, 0, get_ramdisk_size());
 
   return (uintptr_t)DEFAULT_ENTRY;
 }
