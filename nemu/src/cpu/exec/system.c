a9d06cbd4cb90ff4c139d7a5b32af76acc204871
tracer-ics2017 <tracer@njuics.org>
2026-05-30 00:10:20 -0700

    >  compile 2310682 cong Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:10:20 up  5:21,  1 user,  load average: 0.14, 0.11, 0.16 5f163f8ac51cfb6bcdf2dc9e570d70e8a35c3737

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -56,13 +56,16 @@ make_EHelper(int3)
 
 make_EHelper(iret) {
   // TODO();
-  rtl_pop(&cpu.eip);
-  rtl_pop(&cpu.cs);
-  rtl_pop(&t0);
-  memcpy(&cpu.eflags,&t0,sizeof(cpu.eflags));
+  rtl_pop(&cpu.eip);        // 弹出 EIP（返回地址）
+  
+  rtl_pop(&t0);             // 弹出 CS（32位，但只取低16位）
+  cpu.cs = t0;
+  
+  rtl_pop(&t1);             // 弹出 EFLAGS
+  memcpy(&cpu.eflags, &t1, sizeof(cpu.eflags));  // union 类型必须用 memcpy
 
-  decoding.jmp_eip=1;
-  decoding.seq_eip=cpu.eip;
+  decoding.is_jmp = 1;      // 设置跳转标志
+  decoding.jmp_eip = cpu.eip; // 跳转目标为弹出的返回地址
 
   print_asm("iret");
 }
