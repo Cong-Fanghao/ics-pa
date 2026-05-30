ff1da6286b6e48d447b9b87589e4b260d2d2c703
tracer-ics2017 <tracer@njuics.org>
2026-05-30 01:18:26 -0700

    >  compile 2310682 cong Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  01:18:26 up  6:29,  1 user,  load average: 0.09, 0.07, 0.02 35ebfb77b6ad5e6ee41bde5e9781116aa9379713

--- nexus-am/am/arch/x86-nemu/src/asye.c
+++ nexus-am/am/arch/x86-nemu/src/asye.c
@@ -7,7 +7,8 @@ void vecsys();
 void vecnull();
 
 _RegSet* irq_handle(_RegSet *tf) {
-  Log("irq_handle: tf->irq = %d", tf->irq);
+  const char *msg = "irq_handle: ";
+  for (int i = 0; msg[i]; i++) _putc(msg[i]);
   _RegSet *next = tf;
   if (H) {
     _Event ev;
