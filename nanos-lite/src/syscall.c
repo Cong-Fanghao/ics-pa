ccac1bfff4f3b4c99911fe4d7960035f972fc4dc
tracer-ics2017 <tracer@njuics.org>
2026-05-29 23:17:01 -0700

    >  run 2310682 cong Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  23:17:01 up  4:28,  1 user,  load average: 0.00, 0.12, 0.15 c0355e582b3aeffa33d7b356f3bd96fbbfc5900e

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,6 +11,7 @@ void sys_exit(int a){
 
 int sys_write(int fd, void *buf, size_t len)
 {
+  Log("sys_write called: fd=%d, buf=%p, len=%d", fd, buf, len);
   if (fd == 1 || fd == 2)
   {
     char c;
