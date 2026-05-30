29846617a53690aef01acb567f98b46c040131b2
tracer-ics2017 <tracer@njuics.org>
2026-05-30 01:35:56 -0700

    >  compile 2310682 cong Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  01:35:56 up  6:47,  1 user,  load average: 0.03, 0.02, 0.00 846edb774fb7d77f8838cee167ed65f72dfcc52a

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -34,7 +34,9 @@ make_EHelper(mov_cr2r) {
 
 make_EHelper(int) {
   // TODO();
-  raise_intr(id_dest->val, decoding.seq_eip);
+  uint8_t NO = id_dest->val & 0xff;
+  rtl_addi(&decoding.seq_eip, &decoding.seq_eip, 2);  // 关键！
+  raise_intr(NO, decoding.seq_eip);
 
   print_asm("int %s", id_dest->str);
 
