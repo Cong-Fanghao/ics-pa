767c5f925f4345895287dc2dd4c5708658265b3e
tracer-ics2017 <tracer@njuics.org>
2026-05-30 03:37:26 -0700

    >  run 2310682 cong Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  03:37:26 up  8:48,  1 user,  load average: 0.11, 0.09, 0.08 eee9a19822c0cdb7843dc066bc693647df026e9f

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,16 +1,22 @@
 #include "common.h"
+#include "fs.h"
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
 extern size_t get_ramdisk_size();
 
-uintptr_t loader(_Protect *as, const char *filename)
-{
+uintptr_t loader(_Protect *as, const char *filename) {
   // TODO();
-  // ramdisk_read(DEFAULT_ENTRY,0,RAMDISK_SIZE);//PA3.1
+
+  //ramdisk_read(DEFAULT_ENTRY,0,get_ramdisk_size()); 
   int fd = fs_open(filename, 0, 0);
-  Log("filename=%s,fd=%d", filename, fd);
-  fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd)); // 把文件整个读入内存DEFAULT_ENTRY处
+  Log("fd=%d\n",fd);
+  size_t f_size = fs_filesz(fd);
+  //Log("filesize=%d",f_size);
+  fs_read(fd, DEFAULT_ENTRY, f_size);
   fs_close(fd);
+  
   return (uintptr_t)DEFAULT_ENTRY;
+
 }
+
