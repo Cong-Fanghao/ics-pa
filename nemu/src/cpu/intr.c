85527b99d72d6e30c36ae83796c854d60242e984
tracer-ics2017 <tracer@njuics.org>
2026-05-31 04:36:49 -0700

    >  compile 2310682 cong Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  04:36:49 up  2:34,  1 user,  load average: 0.41, 0.48, 0.25 9eb94504e9dfdaf27bef819b893e8cd65be0a93b

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -27,10 +27,5 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
 }
 
 void dev_raise_intr() {
-  const uint8_t IRQ_TIMER = 32;
   cpu.INTR = true;
-  
-  if (cpu.Eflags.IF) {
-    raise_intr(IRQ_TIMER, cpu.eip);
-  }
 }
