3ae28f860ce35b4cd4bf51a9f2718fd9977e01ef
tracer-ics2017 <tracer@njuics.org>
2026-05-30 21:17:23 -0700

    >  run 2310682 cong Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:17:23 up 54 min,  1 user,  load average: 0.15, 0.13, 0.09 a3f8a507e90c0b32f856d2ebebd65715e44cf3f2

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -15,15 +15,17 @@ void load_prog(const char *filename) {
   uintptr_t entry = loader(&pcb[i].as, filename);
 
   // TODO: remove the following three lines after you have implemented _umake()
-  _switch(&pcb[i].as);
-  current = &pcb[i];
-  ((void (*)(void))entry)();
+  // _switch(&pcb[i].as);
+  // current = &pcb[i];
+  // ((void (*)(void))entry)();
 
   _Area stack;
   stack.start = pcb[i].stack;
   stack.end = stack.start + sizeof(pcb[i].stack);
 
   pcb[i].tf = _umake(&pcb[i].as, stack, stack, (void *)entry, NULL, NULL);
+
+  pcb[i].cur_brk = pcb[i].max_brk = 0;
 }
 
 _RegSet* schedule(_RegSet *prev) {
