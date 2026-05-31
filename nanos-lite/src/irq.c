813b867435f8984eba2840d2df697a2624a6d46c
tracer-ics2017 <tracer@njuics.org>
2026-05-30 21:29:27 -0700

    >  run 2310682 cong Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:29:27 up  1:07,  1 user,  load average: 0.34, 0.19, 0.17 4febc1c01204026cb949d47071c2ef406ae5992e

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,7 +5,9 @@ extern _RegSet *schedule(_RegSet *prev);
 
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
-    case _EVENT_SYSCALL: return do_syscall(r);
+    case _EVENT_SYSCALL:
+      do_syscall(r);
+      return schedule(r);
     case _EVENT_TRAP: 
       Log("TRAP event received, calling schedule");
       return schedule(r);
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,16 +28,27 @@ void load_prog(const char *filename) {
   pcb[i].cur_brk = pcb[i].max_brk = 0;
 }
 
-_RegSet* schedule(_RegSet *prev) {
-  if (current != NULL) {
-    current->tf = prev;
+static PCB *select_proc(void) {
+  static int counter = 0;
+  static PCB *last = NULL;
+  
+  PCB *choice = (counter++ % 1000 == 0) ? &pcb[1] : &pcb[0];
+  
+  if (choice != last) {
+    Log("sched: %s -> %s", 
+        last ? (last == &pcb[0] ? "pal" : "hello") : "none",
+        choice == &pcb[0] ? "pal" : "hello");
+    last = choice;
   }
   
-  current = &pcb[0];
-
-  Log("schedule: switching to proc 0, tf at %p", current->tf);
-
-  _switch(&current->as);
-  return current->tf; 
+  return choice;
 }
 
+_RegSet *schedule(_RegSet *prev) {
+  if (current) current->tf = prev;
+  
+  current = select_proc();
+  
+  _switch(&current->as);
+  return current->tf;
+}
